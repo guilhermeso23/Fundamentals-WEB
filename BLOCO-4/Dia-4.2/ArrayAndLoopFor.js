778828b688088dba8146977e392d796e619addd4
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-02-26 19:18:50 -0300

    4. Imprime mensagem

--- BLOCO-4/Dia-4.2/ArrayAndLoopFor.js
+++ BLOCO-4/Dia-4.2/ArrayAndLoopFor.js
@@ -6,3 +6,7 @@ for(let index = 0 ; index < numbers.length ; index += 1) {
 }
 console.log("Soma dos valores: " + sum);
 console.log("Média aritmética: " + (sum / numbers.length));
+if((sum/numbers.length) > 20)
+  console.log("Valor maior que 20");
+  else
+  console.log("Valor menor ou igual a 20");

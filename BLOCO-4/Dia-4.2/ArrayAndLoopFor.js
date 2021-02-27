83095827459c8c641bb2a7afc620b084292d48b5
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-02-26 21:57:30 -0300

    Mostra o maior valor

--- BLOCO-4/Dia-4.2/ArrayAndLoopFor.js
+++ BLOCO-4/Dia-4.2/ArrayAndLoopFor.js
@@ -1,6 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-for(let index = 0 ; index < numbers.length ; index += 1) {
+let aux;
+for(let i = (numbers.length -1) ; i >= 1 ; i -= 1){
+  for(let j = 0 ; j < i ; j += 1){
+    if(numbers[j] > numbers[j+1]){
+      aux = numbers[j];
+      numbers[j] = numbers[j+1];
+      numbers[j+1] = aux;
+    }
+  }
+}
+for(let index = 0 ; index < numbers.length ; index += 1){
   console.log(numbers[index]);
   sum += numbers[index];
 }
@@ -8,5 +18,6 @@ console.log("Soma dos valores: " + sum);
 console.log("Média aritmética: " + (sum / numbers.length));
 if((sum/numbers.length) > 20)
   console.log("Valor maior que 20");
-  else
+else
   console.log("Valor menor ou igual a 20");
+console.log("Maior valor: " + numbers[numbers.length - 1])

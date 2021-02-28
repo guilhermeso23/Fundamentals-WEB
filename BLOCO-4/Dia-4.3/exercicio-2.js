70ac2e9c5f944a03939e04b1dc3883b0eeabcb0a
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-02-28 20:03:40 -0300

    Conclusao do exercicio 2

--- BLOCO-4/Dia-4.3/exercicio-2.js
+++ BLOCO-4/Dia-4.3/exercicio-2.js
@@ -1,9 +1,6 @@
-//Logica de programação e Algotitmo 4.3
 let n = 5;
 let symbol = '';
 for(let horizontal = 0 ; horizontal < n ; horizontal += 1){
     symbol +='*';
-}
-for(let vertical = 0 ; vertical < n; vertical += 1){
     console.log(symbol);
 }

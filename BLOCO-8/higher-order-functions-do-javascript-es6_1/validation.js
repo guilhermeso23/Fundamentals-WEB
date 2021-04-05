528134c90e653d30589a6a794441deb4b93e473e
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-04-05 18:29:17 -0300

    Criado arquivo Validation

---
+++ BLOCO-8/higher-order-functions-do-javascript-es6_1/validation.js
@@ -0,0 +1,11 @@
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const validation = (rightAnswers, studentAnswers, allResult) => {
+  let cont = 0;
+  for (let index)
+}
+
+const allResult = (answer1, answer2) => {
+  
+}
